23cf0d71277514a90c4335cdbcff0ca68312e30f
yuwei <yuwei@houzz.com>
2022-10-24 13:30:48 -0700

    added ts

--- check-serrano-avail.js
+++ check-serrano-avail.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const { result } = require('lodash');
 const _ = require('lodash');
 
 const parse = (json) => {
@@ -10,11 +9,7 @@ const parse = (json) => {
             ["Reserved", "Not Available", undefined],
             a1105
         );
-        // const siteNO = siteData.site;
         const siteNO = siteData.site;
-        // console.log((siteData));
-        // console.log(`${siteNO}: [${a1105}] - [internal no ${siteNum}]`);
-        // console.log(_.get(siteData.availabilities, "2022-11-05T00:00:00Z"));
 
         return {
             siteNO,
@@ -22,7 +17,6 @@ const parse = (json) => {
         }
     })
     return result;
-    // console.log(json);
 }
 
 const alertWhenTrue = (resultMap) => {
@@ -35,14 +29,18 @@ const alertWhenTrue = (resultMap) => {
         return isAvailable;
     })
 
+    var date = Date.now()
+    var date = new Date()
+    const ts = date.toTimeString()
+
     if (availableSites.length > 0)
     {
-        console.log("FOUND AVAILABLE SITES~");
+        console.log(`${ts} FOUND AVAILABLE SITES~`);
         console.log(availableSites);
     }
     else
     {
-        console.log("ALL RESERVED")
+        console.log(`${ts} ALL RESERVED`)
     }
 }
 
@@ -50,18 +48,10 @@ const execute = () => {
     axios.get('https://www.recreation.gov/api/camps/availability/campground/232250/month?start_date=2022-11-01T00%3A00%3A00.000Z')
     // Show response data
     .then(res => {
-        const json = res.data
         const parsedRes = parse(res.data)
-        // console.log(parsedRes)
         alertWhenTrue(parsedRes)
     })
     .catch(err => console.log(err))
 }
 
 setInterval(execute, 5000);
-
-// while(true)
-// {
-//     sleep(5)
-//     execute()
-// }
